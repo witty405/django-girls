cd2b5865be92df7e138825bf5b1e6ffed9ce1f54
witty okechukwu <chiamakaokechukwu682@gmail.com>
2023-10-13 17:05:11 +0100

    firstDjangoproject

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date_created = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date=timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
